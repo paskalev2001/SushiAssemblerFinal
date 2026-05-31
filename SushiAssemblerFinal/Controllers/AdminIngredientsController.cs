8f75d9a3e4be3e216590e5d84dda4bfb76c0ab16
paskalev2001 <2001paskalpaskalev@gmail.com>
2026-05-31 17:21:12 +0300

    CRUD operations for Ingredients and Products

---
+++ SushiAssemblerFinal/Controllers/AdminIngredientsController.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SushiAssemblerFinal.Data;
+using SushiAssemblerFinal.Models;
+
+namespace SushiAssemblerFinal.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminIngredientsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AdminIngredientsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var ingredients = await _context.Ingredients
+                .OrderBy(i => i.Type)
+                .ThenBy(i => i.Name)
+                .ToListAsync();
+
+            return View(ingredients);
+        }
+
+        public IActionResult Create()
+        {
+            return View(new Ingredient
+            {
+                IsAvailable = true
+            });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Ingredient ingredient)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Ingredients.Add(ingredient);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(ingredient);
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var ingredient = await _context.Ingredients.FindAsync(id);
+
+            if (ingredient == null)
+            {
+                return NotFound();
+            }
+
+            return View(ingredient);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Ingredient ingredient)
+        {
+            if (id != ingredient.Id)
+            {
+                return BadRequest();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Ingredients.Update(ingredient);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!await IngredientExists(ingredient.Id))
+                    {
+                        return NotFound();
+                    }
+
+                    throw;
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(ingredient);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var ingredient = await _context.Ingredients
+                .FirstOrDefaultAsync(i => i.Id == id);
+
+            if (ingredient == null)
+            {
+                return NotFound();
+            }
+
+            return View(ingredient);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var ingredient = await _context.Ingredients.FindAsync(id);
+
+            if (ingredient != null)
+            {
+                _context.Ingredients.Remove(ingredient);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        private async Task<bool> IngredientExists(int id)
+        {
+            return await _context.Ingredients.AnyAsync(i => i.Id == id);
+        }
+    }
+}
---
+++ SushiAssemblerFinal/Controllers/AdminProductsController.cs
@@ -0,0 +1,151 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SushiAssemblerFinal.Data;
+using SushiAssemblerFinal.Models;
+
+namespace SushiAssemblerFinal.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminProductsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AdminProductsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var products = await _context.Products
+                .Include(p => p.Category)
+                .OrderBy(p => p.Category!.Name)
+                .ThenBy(p => p.Name)
+                .ToListAsync();
+
+            return View(products);
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            await PopulateCategoriesDropDownList();
+
+            var product = new Product
+            {
+                IsAvailable = true
+            };
+
+            return View(product);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Products.Add(product);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            await PopulateCategoriesDropDownList(product.CategoryId);
+
+            return View(product);
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            await PopulateCategoriesDropDownList(product.CategoryId);
+
+            return View(product);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Product product)
+        {
+            if (id != product.Id)
+            {
+                return BadRequest();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Products.Update(product);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!await ProductExists(product.Id))
+                    {
+                        return NotFound();
+                    }
+
+                    throw;
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            await PopulateCategoriesDropDownList(product.CategoryId);
+
+            return View(product);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var product = await _context.Products
+                .Include(p => p.Category)
+                .FirstOrDefaultAsync(p => p.Id == id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            if (product != null)
+            {
+                _context.Products.Remove(product);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        private async Task PopulateCategoriesDropDownList(object? selectedCategory = null)
+        {
+            var categories = await _context.Categories
+                .OrderBy(c => c.Name)
+                .ToListAsync();
+
+            ViewBag.CategoryId = new SelectList(categories, "Id", "Name", selectedCategory);
+        }
+
+        private async Task<bool> ProductExists(int id)
+        {
+            return await _context.Products.AnyAsync(p => p.Id == id);
+        }
+    }
+}
